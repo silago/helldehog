f74070005ea82b5dde3728e1fe04eb45c4898428
silago <silago.nevermind@gmail.com>
2016-03-16 14:24:19 +0800

    quest

---
+++ addons/godot_tiled_importer/importer_plugin.gd
@@ -0,0 +1,23 @@
+tool
+extends EditorPlugin
+
+var button = null
+var window = null
+
+func get_name():
+	return "Tiled Importer"
+
+func _enter_tree():
+	button = Button.new()
+	button.set_text("Import Tiled Map")
+	button.connect("pressed",self,"_clickedButton")
+	#add_custom_control(CONTAINER_TOOLBAR,button)
+	add_control_to_dock( DOCK_SLOT_LEFT_UL, button)
+
+func _exit_tree():
+	button.free()
+	button = null
+
+func _clickedButton():
+	window = preload("plugin.xml")
+	add_child(window.instance())
--- globals.gd
+++ globals.gd
@@ -2,7 +2,6 @@ extends Node
 
 var camera
 var quests = []
-var quest_objects = {}
 var quest_vars = {
 	'BUTTERLYCATCHED':0,
 	'BUTTERLYTOCATCH':1
@@ -17,155 +16,14 @@ var waiting_to_change = false
 var main 
 var next_scene
 
-var quest_requirements = {
-	'quest_1':	{
-		'helldehog':['PlayerReady'],
-		'ыгзукрщпsuperhog' :['PlayerSuperhogCollided'],
-		'gui':[],
-		'butterfly1':['PlayerButterflyCollided'],
-		'motherhog':['PlayerMotherhogCollided'],
-		'smallhog2':['PlayerSmallHogCollided'],
-		'smallhog3':['PlayerSmallHogCollided'],
-		'branch':['PlayerBranchCollidedSpacePressed'],
-		'branch_place':['PlayerSmallHogCollidedSpacePressed'],
-		'oldhog':['PlayerOldhogCollided'],
-		'stick':['PlayerStickCollidedSpacePressed'],
-		'pit':['зешPlayerPitEnter'],
-	}
-}
 
 
 const SIGNAL_CALLER = 0
+var    quest_objects={}
+var    quest_data =  {}
 
-var    quest_data =  {
-		'PlayerReady':{
-			'TALK_TO_SUPERHOG':[
-				['SAY',[tr('INTRO_1'),tr('INTRO_2'),tr('INTRO_3'),tr('INTRO_4')]],
-			]
-		},
-		'PlayerMotherhogCollided': {
-			'CATCH_HOGS':[
-				['SET_STATE',['FIND_SMALL']],
-				['SAY',[tr('MOTHERHOG_1'),tr('MOTHERHOG_2')]]
-			],
-			'SMALLHOG_SAVED':[
-				['SAY',[tr('MOTHERHOG_4')]],
-				['SET_STATE',['ALL_SAVED']],
-				['SET_NPC_STATE',['smallhog2',0]],
-				['SET_NPC_STATE',['smallhog3',0]],
-				['SET_NPC_STATE',['smallhog4',0]],
-			],
-			'FIND_BRANCH':[
-				['SAY_ONCE',[tr('MOTHERHOG_3')]]
-			]
-		},
-		'PlayerSmallHogCollided': {
-			'FIND_SMALL':[
-				['SAY',[tr('SMALLHOG_1'),tr('SMALLHOG_2'),tr('SMALLHOG_3')]],
-				['SET_STATE',['FIND_BRANCH']]
-			],
-			'BRANSH_PUSHED':[
-				['SAY',[tr('SMALLHOG_4'),tr('SMALLHOG_5')]],
-				['SET_STATE',['SMALLHOG_SAVED']]
-			],
-			'SMALLHOG_SAVED':[
-			]
-		},
-		'зешPlayerPitEnter':{
-			'TALK_TO_SUPERHOG':[
-				['GAMEOVER_FAIL',['']],
-				['SAY',[tr('GAMEOVER_1')]],
-			],
-			'CATCH_HOGS':[
-				['GAMEOVER_FAIL',['']],
-				['SAY',[tr('GAMEOVER_1')]],		
-			],
-			'FIND_BRANCH':[
-				['GAMEOVER_FAIL',['']],
-				['SAY',[tr('GAMEOVER_1')]],		
-			],
-			'FINDSMALL':[
-				['GAMEOVER_FAIL',['']],
-				['SAY',[tr('GAMEOVER_1')]],		
-			]
-		},
-		'PlayerBranchCollidedSpacePressed': {
-			'FIND_BRANCH':[
-				['HIDE',['branch']],
-				['SET_STATE',['GOT_BRANCH']]
-			]
-		},
-		'PlayerSmallHogCollidedSpacePressed': {
-			'GOT_BRANCH':[
-				['GET_POS',['smallhog4']],
-				['SET_POS',['branch']],
-				['SET_ROT',['branch',-0.4]],
-				['SHOW',['branch']],
-				['SET_STATE',['BRANSH_PUSHED']],
-				['SET_NPC_STATE',['smallhog2',1]],
-				['SET_NPC_STATE',['smallhog3',1]],
-				['SET_NPC_STATE',['smallhog4',1]],
-			]
-		},
-		'PlayerOldhogCollided':{
-			'SMALLHOG_SAVED':[
-			   ['SAY',[
-					tr('OLDHOG_1')
-					]],
-			   ['SET_STATE',['FIND_STICK']]
-			],
-			'FOUND_STICK':[
-			   ['SAY',['good']],
-			]
-		},
-		'PlayerStickCollidedSpacePressed':{
-			'FIND_STICK':[
-			   ['SET_STATE',['FOUND_STICK']],
-			   ['HIDE',['stick']],
-			]
-		},
-        'PlayerSuperhogCollided': {
-			'ALL_SAVED':[
-			  ['SAY',[
-					tr('OLDHOG_6'),
-					tr('OLDHOG_7'),
-					tr('OLDHOG_8'),
-					]
-				],
-				['GAMEOVER_WIN',['']],
-			],
-            'TALK_TO_SUPERHOG':[
-                ['SAY',[
-					tr('OLDHOG_1'),
-					tr('OLDHOG_2'),
-					tr('OLDHOG_3'),
-					tr('OLDHOG_4')
-					]
-				],
-                ['SET_STATE',['CATCH_HOGS']],
-            ]
-        },
-        'PlayerButterflyCollided':{
-            'CATCH_BUTTERFLIES':[
-                ['INC_VAR',['BUTTERLYCATCHED']],
-                ['REMOVE',[]],
-				['EMIT_SIGNAL_IF_EQUAL',['BUTTERLYCATCHED',1,'AllbutterfliesCatched']]
-            ]
-        },
-		'AllbutterfliesCatched':{
-			'CATCH_BUTTERFLIES':[
-			    ['SET_STATE',['ALLCATCHED']],
-				['PUSH_QUERY',['helldehog','RUN_JUMP_RIGHT']]
-			]
-		},
-		'PlayerFallerCollided':{
-			'ALLCATCHED':[
-				['SET_STATE',['FALLED']],
-				['CLEAR_QUERY',['helldehog','RUN_JUMP_RIGHT']]
-			]
-		}
-    }
-var STATE = 'TALK_TO_SUPERHOG';
+var STATE = 'TALK_TO_SUPERHOG'
+#var STATE = 'QUEST_DONE'
 func set_camera(c):
 	self.camera = c
 func get_camera():
@@ -228,6 +86,8 @@ func signal_resolver(sig_name,caller = null):
 					save_game()
 				if (action == 'SET_NPC_STATE'):
 					quest_objects[action_data[0]].set_state(int(action_data[1]))
+				if (action == 'CHANGE_SCENE'):
+					change_scene('res://'+action_data[0])
 func old_save_game():
 	pass
 	# objects to save:
@@ -236,81 +96,24 @@ func old_save_game():
 	#	3) checkpoint_id
 	#var save = current_mape & game_state_id & checkpoint_id 
 
-func add_to_quest_objects(name,obj):
+func add_to_quest_objects(name,obj,signals=[]):
 	#print(name)
+	#if (signals.size()==0):
+	#	print("!!!!!")
+	#	print(name)
 	quest_objects[str(name)]=obj
-	for q in quest_requirements:
-		if name in quest_requirements[q]:
-			for s in quest_requirements[q][name]:
-				print('connect')
-				print(s)
-				obj.connect(s,self,'signal_resolver')
-			quest_requirements[q].erase(name)
-		else:
-			print(name+" not found")
-		if quest_requirements[q].size() == 0:
-			call(q+'_prepare')
-			quest_requirements.erase(q)
-	#print(quest_requirements)
-		
-func quest_1(ev,v = false):
+	for s in signals:
+		obj.connect(s,self,'signal_resolver')
 	return
-	if (ev == 'nowayCollided' and not quest_vars['quest_1_got']):
-		quest_objects['gui'].say('I must talk to Hogge first.')
-		var player = quest_objects['helldehog']
-		var lv = player.get_linear_velocity()
-		lv.x = lv.x*-1
-		lv.y = lv.y*-1
-		player.set_linear_velocity(lv)
-	if (ev == 'superhogCollided'):
-		quest_objects['gui'].say('Hello. Here is a wolf in the forest. Go and catch some butterflies and we shall go away from here.')
-		quest_vars['quest_1_got']=true
-		if (quest_objects.has('noway')):
-			quest_objects['noway'].queue_free()
-			quest_objects.erase('noway')
-	if (ev == 'butterflyCollided' and quest_vars['quest_1_got']):
-		quest_vars['butterfliesLeftToCatch']-=1
-		if quest_vars['butterfliesLeftToCatch']==0:
-			quest_objects[v].catched()
-			quest_objects['gui'].say('I\'m a good hog. I\'ve catched all I need. ..... AAAAAAAAAAA!!!!!!!')
-			quest_objects['helldehog'].apply_impulse(Vector2(100,-20),Vector2(250,-50))
-			quest_objects['ыгзукрщпsuperhog'].queue_free()
-
-func quest_1_prepare():
-	#quest_vars['quest_1_got']=false
-	#quest_objects['ыгзукрщпsuperhog'].connect('superhogCollided',self,'quest_1',['superhogCollided'])
-	#quest_objects['noway'].connect('nowayCollided',self,'quest_1',['nowayCollided'])
-	#quest_objects['butterfly1'].connect('ыгзукрщпbutterflyCollided',self,'quest_1',['butterflyCollided','butterfly1'])
-	pass
 	
 
-
-#func prepare_quests():
-#	quest_1_prepare()
-#	pass	
-
-
-#func send_signal(sig):
-#	for listener in listeners:
-#		if (listener.has_method('get_event')):
-#			pass
-
-func some_function():
-	print("some function called")
 
 func _ready():
 	set_pause_mode(3)
 	set_process_input(true)
 	for i in get_tree().get_root().get_children():
 		print(i.get_name())
-	#prepare_quests()
-	print("globals ready")
 	main = Node2D.new()
-	#key_chaims_sound = StreamPlayer.new()
-	#key_chaims_sound.set_stream(load("res/music/KeyChimes.ogg"))
-	#key_chaims_sound.play()
-	
-	#KeyChimes.ogg
 	
 func get_json_file(path):
 	var json = File.new()
@@ -335,13 +138,9 @@ func save_game():
 		"scene":"res://scenes/water_scene.scn",
 		"x": pos.x,
 		"y": pos.y,
-		#"state":STATE,
 		"state":STATE,
 		"stack":stack
 		}
-	#var savenodes = get_tree().get_nodes_in_group("Persist")
-	#for i in savenodes:
-	#	var nodedata = i.save()
 	savegame.store_line(savedata.to_json())
 	savegame.close()
 	if !savegame.file_exists("user://savegame.save"):
@@ -384,6 +183,10 @@ func _process(delta):
 			set_process(false)
 			change_scene(next_scene)
 			
+func load_quest_data(path):
+	self.quest_data = get_json_file(path)
+	#print(quest_data)
+	
 func change_scene(path):
 	get_tree().set_pause(true)
 	get_tree().get_root().get_node(".").get_node("container").set_name(".container")
